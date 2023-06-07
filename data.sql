1b68aff30241aea2370f52a86f25a8af2536fc6b
yacoubou-seidou <yacoseidou@gmail.com>
2023-06-07 21:54:43 +0100

    Make required changes

--- data.sql
+++ data.sql
@@ -7,6 +7,7 @@ INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) V
 INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Gabumon','2018-11-05', 8, true, 2);
 INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Pikachu','2021-01-07', 15.04, false, 1);
 INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Devimon','2017-05-12', 11, true, 5);
+ALTER TABLE animals ADD COLUMN species VARCHAR;
 INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Charmander','2020-02-08', -11, false, 0);
 INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Plantmon','2021-11-15', -5.7, true, 2);
 INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Squirtle','1993-04-02', -12.13, false, 3);
